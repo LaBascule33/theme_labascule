54f6b1c6ce8c1426cc390fa431c064b23ab2ae12
fcousin <fredc94@gmail.com>
2020-01-30 15:34:21 +0100

    update&merge

--- pages_templates/rejoindre.php
+++ pages_templates/rejoindre.php
@@ -5,19 +5,19 @@
 <div id="pRejoindre" class="container">
     <div class="row">
         <div class="col-6 bg-title-page position-relative">
-            <h3 class="bf page-title">Nous rejoindre</h3>
+            <h1 class="bf page-title">Nous rejoindre</h1>
         </div>
 
 <!----------------------------------------------------------------------------------------------->
 <!-- Je rejoins la BASCULE -->
 <!------------------------------------->
-        <div class="col-6 text-asso-row">
+        <div class="col-6 block-text-title">
             
-            <h1 class="bf heading">Je rejoins <span class="text-uppercase bc">LA BASCULE</span></h1>
+            <h2 class="bf">Je rejoins <span class="bc">la <span class="text-uppercase">BASCULE</span></span></h2>
 
-            <p>Je souhaite mettre mon énergie <span class="bf-under">au service des projets</span> qui vont amplifier la bascule démocratique, écologique et solidaire, participer à <span class="bf-under">la reprise en main par le citoyen du pouvoir d’agir et de décider.</span></p>
-            <p>J’ai envie de contribuer à la mise en œuvre de solutions respectueuses du vivant et de l’humain en incarnant ces notions au mieux et de m’engager dans <span class="bf-under">une dynamique collective.</span></p>
-            <p><span class="bf-under">Étudiant, retraité, salarié, chômeur, artiste, ouvrier, scientifique, professeur, citoyen,... en quête de sens et d’action ?</span> Je peux devenir un acteur de cet élan collectif en rejoignant la BASCULE.</p>
+            <p>Je souhaite mettre mon énergie <span class="bf">au service des projets</span> qui vont amplifier la bascule démocratique, écologique et solidaire, participer à <span class="bf">la reprise en main par le citoyen du pouvoir d’agir et de décider.</span></p>
+            <p>J’ai envie de contribuer à la mise en œuvre de solutions respectueuses du vivant et de l’humain en incarnant ces notions au mieux et de m’engager dans <span class="bf">une dynamique collective.</span></p>
+            <p><span class="bf">Étudiant, retraité, salarié, chômeur, artiste, ouvrier, scientifique, professeur, citoyen,... en quête de sens et d’action ?</span> Je peux devenir un acteur de cet élan collectif en rejoignant la BASCULE.</p>
             <!--
             <div class="row pt-4">
                 <a href="" class="pl-3"><i class="fas fa-download bc"></i> <span class="bf">Télécharger la présentation</span></a>
@@ -27,89 +27,24 @@
     </div>
 
 <!----------------------------------------------------------------------------------------------->
-<!-- JE POSTULE -->
+<!-- JE POSTULE-->
 <!------------------------------------->
-    <div class="row">
-        <div class="col-6 text-asso-row">
-            <h3 class="bf heading text-uppercase">JE POSTULE</h3>
+    <div id="bloc-postule" class="row">
+        <div class="col-6 block-text">
+            <h3 class="bf heading">JE POSTULE</h3>
 
             <p>Nous recrutons dans tous ces pôles :</p>
 
             <ul>
-                <li><span class="bf text-uppercase font-weight-bold">Agir</span> au service des solutions existantes</li>
-                <li><span class="bf text-uppercase font-weight-bold">Apprendre</span> à vivre et faire ensemble</li>
-                <li><span class="bf text-uppercase font-weight-bold">Grandir</span> son engagement</li>
+                <li><i class="arrow-right fas fa-arrow-right bc mr-2"></i> <span class="bf text-uppercase">Agir</span> au service des solutions existantes</li>
+                <li><i class="arrow-right fas fa-arrow-right bc mr-2"></i> <span class="bf text-uppercase">Apprendre</span> à vivre et faire ensemble</li>
+                <li><i class="arrow-right fas fa-arrow-right bc mr-2"></i> <span class="bf text-uppercase">Grandir</span> son engagement</li>
             </ul>
 
-            <!--<p>La Bascule a pour ambition de <span class="bf">pousser notre société vers un modèle viable et respectueux de la nature et de l'Homme</span></p>
-            <p>Nous agissons pour:</p>
-            <ul class="list-style-custom">
-                <li><i class="arrow-right fas fa-arrow-right bc mr-2"></i> <span class="bf">L'évolution de nos consciences</span> vers plus d'empathie avec le vivant dont nous faisons partie ainsi que les générations futures</li>
-                <li><i class="arrow-right fas fa-arrow-right bc mr-2"></i> <span class="bf">L'amplification des solutions</span> existantes qui permettent une transition démocratique, écologique et solidaire</li>
-                <li><i class="arrow-right fas fa-arrow-right bc mr-2"></i> <span class="bf">La protection du vivant et de sa génération</span></li>
-                <li><i class="arrow-right fas fa-arrow-right bc mr-2"></i> La réappropriation par le citoyen du <span class="bf">pouvoir d'agir et de décider</span></li>
-            </ul>-->
-
         </div>
 
-        <div class="col-6 bgj">
-            <div class="position-relative bloc-archipel">
-                <img class="position-absolute img-archipel" src="<?php echo get_template_directory_uri(); ?>/img/archipel.jpg" alt="img_archipel"/>
-            </div>
-        </div>
-<!--
-        <div class="col-6 bgbc">
-
-            <div class="p-4">
-                <div class="row pt-4 pb-3">
-                    <div class="col-3">
-                        <img src="<?php echo get_template_directory_uri(); ?>/img/gouvernance-partagee.svg" alt="logo gouvernance">
-                    </div>
-                    <div class="col-9">
-                        <span class="bf">Gouvernance partagée</span>
-                        <p>Fonctionner en gouvernance partagée pour aller <span class="font-weight-bold">du pouvoir sur</span> des personnes <span class="font-weight-bold">au pouvoir de</span> faire avec laisser place à la puissance de l'intelligence collective.</p>
-                    </div>
-                </div>
-
-                <div class="row pt-4 pb-3">
-                    <div class="col-3">
-                        <img src="<?php echo get_template_directory_uri(); ?>/img/mobilisation.svg" alt="logo force de mobilisation">
-                    </div>
-                    <div class="col-9">
-                        <span class="bf">Force de mobilisation</span>
-                        <p>Mobiliser un maximum de citoyens.</p>
-                    </div>
-                </div>
-
-                <div class="row pt-4">
-                    <div class="col-3">
-                        <img src="<?php echo get_template_directory_uri(); ?>/img/independance-financiere.svg" alt="logo independance financiere">
-                    </div>
-                    <div class="col-9">
-                        <span class="bf">Indépendance financière</span>
-                        <p>Atteindre l'indépendance financière dans notre fonctionnement au quotidien.</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    -->
-    </div>
-
-
-<!----------------------------------------------------------------------------------------------->
-<!-- LA VIE DANS UN QUARTIER DE BASCULE -->
-<!------------------------------------->
-    <div class="row">
-        <div class="col-6">
-            <img src="" alt="visuel de l'imaginaire">
-        </div>
-        <div class="col-6 text-asso-row">
-            <h3 class="bf text-uppercase">Une démarche personnelle<br>d'engagements</h3>
-            <p>pour faire la bascule avec ou sans nous</p>
-            
-            <div class="pt-4">
-            <a href="" class="btn btn-primary">En savoir +</a>
-            </div>
+        <div class="col-6 bg-img">
+            &nbsp;
         </div>
     </div>
 
@@ -117,98 +52,86 @@
 <!----------------------------------------------------------------------------------------------->
 <!-- En m’engageant dans un Quartier de Bascule : -->
 <!------------------------------------->
-    <div class="row">
-        <div class="col-6 text-asso-row">
-            <h3 class="bf text-uppercase">Une philosophie de coopération</h3>
-            <p>pour que nous, citoyens et organisations, puissions bâtir une unité dans nos diversités.</p>
-
-            <div class="pt-4">
-            <a href="" class="btn btn-primary">Plonger dans l'archipel</a>
-            </div>
+    <div  id="bloc-engagement" class="row">
+        <div class="col-6 bg-img">
+            &nbsp;
         </div>
+
+        <div class="col-6 block-text">
+            <h3 class="bf">En m’engageant dans un Quartier de Bascule :</h3>
+
+            <ul>
+                <li>Je participe à des projets amplifiant la bascule démocratique, écologique et solidaire</li>
+                <li>Je participe à la vie de l’association La Bascule. Je me forme à de nombreux outils et modes d’actions pour renforcer mon engagement pour la Transition: sensibilisation, posture de coopération, facilitation, désobéissance civile, gouvernance partagée, bilan carbone, etc.</li>
+                <li>Je vis une expérience fondatrice de Transition. Il y aura un « avant » et un « après » cette expérience.</li>
+            </ul>
+
+        </div>
+    </div>
+
+
+<!----------------------------------------------------------------------------------------------->
+<!-- LA VIE DANS UN QUARTIER DE BASCULE -->
+<!------------------------------------->
+    <div id="bloc-QB" class="row">
+        
+        <div class="col-6 block-text">
+            <h3 class="bf">LA VIE DANS UN QUARTIER DE BASCULE</h3>
+            <p>Un lieu accueillant des volontaires à temps plein pour</p>
+
+            <ul>
+                <li><span class="bf text-uppercase">Agir</span> au service des solutions existantes</li>
+                <li><span class="bf text-uppercase">Apprendre</span> à vivre et faire ensemble</li>
+                <li><span class="bf text-uppercase">Grandir</span> son engagement</li>
+            </ul>
+
+            <p>Chaque lieu est un espace de vie-travail-transformation qui offre une expérience de Transition fondatrice dans la vie des volontaires. Le lieu fonctionne en auto-gestion et en gouvernance partagée. Il développe un modèle économique pour assurer sa pérennité et son indépendance. Chaque lieu est ouvert sur son territoire d’implantation et investi dans une dynamique locale.</p>
+            <p>Il existe deux types de lieux :</p>
+            
+            <ul>
+                <li>Le Quartier de Bascule regroupant 20 à 100 volontaires, dédié à des actions d’échelles transverses, au sein duquel vie en sobriété, formations, soutien de projets d’ampleur et débats citoyens rythment les journées.</li>
+                <li>L’îlot de la Transition hébergeant 3 à 8 volontaires est un espace de vie et de travail plus petit et concentré uniquement sur le soutien d’initiatives locales.</li>
+            </ul>
+                
+        </div>
+
         <div class="col-6 bgj">
             <div class="position-relative bloc-archipel">
                 <img class="position-absolute img-archipel" src="<?php echo get_template_directory_uri(); ?>/img/archipel.jpg" alt="img_archipel"/>
             </div>
+            <p>Après avoir réhabilité l’ancienne polyclinique de Pontivy, y avoir vécu et travaillé de mars à décembre, nous avons aujourd’hui déménagé dans un nouveau quartier de Bascule à Plouray (56). Notre ambition est d’ouvrir de nouveaux lieux similaires d’ici fin 2020 afin d’accueillir des nouveaux volontaires. Nous recrutons les prochaines promotions pour investir ensemble plusieurs lieux que nous avons déjà trouvé.</p>
         </div>
-    </div>
 
-
-<!-- //////////// -->
-
-
-<!----------------------------------------------------------------------------------------------->
-<!-- UNE EQUIPE -->
-<!------------------------------------->
-    <div class="row">
-        <div class="col-6">
-            <img src="" alt="archipel">
-        </div>
-        <div id="section-equipe" class="col-6 text-asso-row">
-            <h3 class="bf text-uppercase">Une équipe</h3>
-            <p>portée par une jeunesse qui a su bien s'entourer.</p>
-            <p><span class="bf">Des citoyens de tout âges</span> en quête de sens qui apportent recul et expériences.</p>
-            <p><span class="bf">Des professionnels engagés de tout horizons</span> qui donnent de leur temps pour transmettre leur savoir-faire.</p>
-
-            <div class="pt-4">
-            <a href="" class="btn btn-primary">Faire connaissance avec l'équipe</a>
-            </div>
-        </div>
     </div>
 
 <!----------------------------------------------------------------------------------------------->
-<!-- DES LIEUX DE BASCULE -->
-<!------------------------------------->
-    <div class="row">
-        <div class="col-6 text-asso-row">
-            <h3 class="bf"><span class="text-uppercase">Des lieux</span> de bascule</h3>
-            <p>Espaces uniques de vie-travail-transformation, accueillant pendant plusieurs semaines des volontaires à plein temps pour:</p>
-            <ul>
-                <li><span class="bf text-uppercase font-weight-bold">Agir</span> au service des solutions existantes</li>
-                <li><span class="bf text-uppercase font-weight-bold">Apprendre</span> à vivre et faire ensemble</li>
-                <li><span class="bf text-uppercase font-weight-bold">Grandir</span> son engagement</li>
-            </ul>
-
-            <div class="pt-4">
-            <a href="" class="btn btn-primary">En savoir +</a>
-            </div>
-        </div>
-        <div class="col-6 text-asso-row">
-            <h3 class="bf"><span class="text-uppercase">Une ambition</span> d'<span class="text-uppercase">implantations</span> sur le territoire</h3>
-            <p>Notre objectif:</p>
-            <p>passer en deux ans de 3 à 30 lieux de bascule répartis sur autant de communes différentes; et d'une équipe de 50 à 300 volontaires à temps plein, secondés sur le territoire par de nombreux citoyens engagés.</p>
-
-            <div class="pt-4">
-            <a href=""><i class="fas fa-download bc"></i> <span class="bf">Voir notre projet de développement</span></a>
-            </div>
-        </div>
-    </div>
-
-
-<!----------------------------------------------------------------------------------------------->
-<!-- CHIFFRES CLEFS -->
+<!-- JE M'ENGAGE -->
 <!------------------------------------->
     <div class="container my-5">
-        <h1 class="heading text-center bf my-5">Chiffres clés</h1>
+        <h2 class="text-center text-uppercase bf my-5" style="margin-bottom: 10px !important;">Je m’engage dans l’élan de bascule</h2>
+        <div class="row" style="justify-content: center; margin-bottom: 3rem;">
+            <p style="font-style: italic;">De nombreuses alternatives existent !</p>
+        </div>
 
         <div class="row">
             <div class="col-3 col-chiffres text-center">
                 <img class="p-auto picto-chiffres" src="<?php echo get_template_directory_uri(); ?>/img/citoyens.svg" alt="petit logo top">
-                <h1 class="bf pt-4">000</h1>
+                <h3 class="bf pt-4">Rejoindre NOS &Eacute;QUIPES LOCALES</h3>
                 <p><span class="bf">Citoyens</span> passés dans un lieu de bascule</p>
             </div>
             <div class="col-3 col-chiffres text-center">
                 <img class="p-auto picto-chiffres" src="<?php echo get_template_directory_uri(); ?>/img/lieux.svg" alt="petit logo top">
-                <h1 class="bf pt-4">3</h1>
+                <h3 class="bf pt-4">Soutenir la BASCULE à ma manière</h3>
                 <ul class="list-unstyled text-center">
-                    <li><span class="bf">Joigny</span> (89), à l'echelle nationale</li>
-                    <li><span class="bf">Plouray</span> (22), à l'echelle régionale</li>
-                    <li><span class="bf">Rennes</span> (22), à l'echelle locale</li>
+                    <li>Je signale un lieu pour devenir un futur lieu de Bascule</li>
+                    <li>Je relaie des actions sur mon réseau</li>
+                    <li>J’offre mes compétences sur des missions précises et ponctuelles</li>
+                    <li>Je finance le mouvement</li>
                 </ul>
             </div>
             <div class="col-3 col-chiffres text-center">
                 <img class="p-auto picto-chiffres" src="<?php echo get_template_directory_uri(); ?>/img/event.svg" alt="petit logo top">
-                <h1 class="bf pt-4">000</h1>
+                <h3 class="bf pt-4">Rejoindre un GROUPE LOCAL</h3>
                 <p><span class="bf">Evénements animés</span> dans le cadre de</p>
                 <ul class="list-unstyled">
                     <li>*formations*</li>
@@ -218,17 +141,12 @@
             </div>
             <div class="col-3 col-chiffres text-center">
                 <img class="p-auto picto-chiffres" src="<?php echo get_template_directory_uri(); ?>/img/organisations.svg" alt="petit logo top">
-                <h1 class="bf pt-4">000</h1>
-                <p><span class="bf">Organisations</span> différentes ont participé à un de vos événements</p>
+                <h3 class="bf pt-4">Rejoindre un LIEU</h3>
+                <p>D’autres lieux sur Transiscope</p>
             </div>
         </div>
     </div>
 
 </div>
-
-
-
-
-<?php get_footer();?>
 
 <?php get_footer();?>
