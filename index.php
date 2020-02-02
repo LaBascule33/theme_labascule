c24310279e155dabe2c1387cb8c0f4e9f9ec78c0
JohnVy <contact@spiritvy.com>
2020-02-02 22:54:30 +0100

    nos actions

--- index.php
+++ index.php
@@ -43,7 +43,7 @@
     <div class="row">
         <div class="col-3 border-right card-body text-center">
             <div class="bg-white mt-0">
-                <h1 class="text-uppercase text-center success">Service de<br />La Transition</h1>
+                <h2 class="text-uppercase text-center success">Service de<br />La Transition</h2>
                 <p class="text-center mb-5 position-relative">Pour permettre à chacun de vivre une expérience de Transition fondatrice <br />dans un de nos lieux de bascule.</p>
                 <button type="button" class="btn btn-success">En lire +</button>
             </div>
@@ -51,7 +51,7 @@
         
         <div class="col-3 border-right card-body text-center">
             <div class="bg-white mt-0">
-                <h1 class="text-uppercase text-center warning">Coopération<br />& événements</h1>
+                <h2 class="text-uppercase text-center warning">Coopération<br />& événements</h2>
                 <p class="text-center mb-5 position-relative">Pour faciliter le passage à l’action <br />collective via du suivi, de la mise en <br />réseau et l’animation de temps fort.</p>
                 <button type="button" class="btn btn-warning">En lire +</button>
             </div>
@@ -59,7 +59,7 @@
         
         <div class="col-3 border-right card-body text-center">
             <div class="bg-white mt-0">
-                <h1 class="text-uppercase text-center primary">Lobbying<br />& citoyen</h1>
+                <h2 class="text-uppercase text-center primary">Lobbying<br />& citoyen</h2>
                 <p class="text-center mb-5 position-relative">Pour nous donner les moyens<br />d'interpeller nos proches, nos <br />organisations et nos élus.</p>
                 <button type="button" class="btn btn-primary">En lire +</button>
             </div>
@@ -67,7 +67,7 @@
         
         <div class="col-3 border-right card-body text-center">
             <div class="bg-white mt-0">
-                <h1 class="text-uppercase text-center danger">Soutien aux<br />Mobilisations</h1>
+                <h2 class="text-uppercase text-center danger">Soutien aux<br />Mobilisations</h2>
                 <p class="text-center mb-5 position-relative">Pour en faire de véritables leviers de <br />Transition à travers un appui humain,<br /> logistique et de communication.</p>
                 <button type="button" class="btn btn-danger">En lire +</button>
             </div>
@@ -157,7 +157,7 @@
 <!-- MOI DANS TOUT ÇA -->
 <!------------------------------------->
 <div id="section-etmoi" class="container my-5">
-    <h2 class="text-center bf"><span class="text-uppercase">& moi</span> dans tout ça ?</span></h2>
+    <h1 class="text-center bf"><span class="text-uppercase">& moi</span> dans tout ça ?</span></h1>
 
     <nav class="nav-home-moi">
         <div class="nav nav-tabs my-5 text-center" id="nav-tab" role="tablist">
@@ -210,7 +210,7 @@
 <!-- UNE PASERELLE -->
 <!------------------------------------->
 <div class="container my-5">
-        <h2 class="text-center bf">Une <span class="text-uppercase">passerelle</span> vers des solutions existantes</h2>
+        <h1 class="text-center bf">Une <span class="text-uppercase">passerelle</span> vers des solutions existantes</h1>
             <!--<h2 class="heading text-center bf">vers des solutions existantes</h2>-->
 
 
