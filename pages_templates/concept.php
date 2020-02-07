a29f5ca37b500b88015ba6c510b52c14a499fefa
JohnVy <contact@spiritvy.com>
2020-02-07 04:24:29 +0100

    rotateblocktest

--- pages_templates/concept.php
+++ pages_templates/concept.php
@@ -1,3 +1,5 @@
+<!---------------------------------------------------------------------------->
+<!-- HEADER -->
 <?php 
 /**
  * Template name: page_concept
@@ -6,65 +8,336 @@
  */
 
 get_header();?>
-<!-- Page est le template destiné à afficher la page le concept -->
-<!-- content -->
+<!---------------------------------------------------------------------------->
 
-<div class="container">
+<!---------------------------------------------------------------------------->
+<!-- NOS ACTIONS -->
+<!------------------------------------->
+<!-- Heading -->
+<!---------------------------------------------------------------------------->
 
-    <div class="background-img">
-        <div class="row">
-            <div class="col-6">
-                <p>"Etre au service de tous ceux qui s'engagent pour un futur souhaitable. Parce que l'utopie est devenue une obligation."</p>
-                <p>Jean-Pierre Dupond</p>
+<section id="category" role="main">
+    <div class="content">
+
+            <!------------------------------------->
+            <!-- La Bascule, c'est quoi -->
+            <!------------------------------------->
+            <div class="block-1">
+                <div class="block d-md-flex">
+                    <div class="image img-asso">
+                        <div class="heading">
+                            <h2>L'association</h2>
+                        </div>
+                    </div>
+
+                    
+                    <div class="text">
+                        <h2 class="mt-4">La <span class="text-uppercase">BASCULE,</span> <span class="bc">c'est quoi ?</span></h2>
+                
+                        <div class="mt-4">
+                            <p>La Bascule est un mouvement citoyen lancé en <span class="bf">février 2019</span>.</p>
+                            <p>Il a pour objectif, face à la <span class="bf">gravité  de la situation</span>
+                            , de pousser notre société vers un modèle viable et respectueux de la nature et de l'Homme.</p>
+                            <p><span class="bf">Fonctionnant en gouvernance partagée</span>, et constitué d'
+                            <span class="bf">une équipe</span> de citoyens engagés à temps plein autour desquels gravitent 
+                            <span class="bf">de nombreux acteurs</span> des transitions écologique, sociale et démocratique, la Bascule déploie un 
+                            <span class="bf">programme d'actions</span> ambitieux au service de l'évolution des consciences, l'amplification des solutions existantes, 
+                            la protection du vivant et la réappropriation par le citoyen du pouvoir d'agir et de décider.</p>
+                        </div>
+                
+                        <div class="row mt-5">
+                            <a href="" class="pl-3"><i class="fas fa-download bc pr-2"></i> <span class="bf">Télécharger la présentation</span></a>
+                            <a href="" class="pl-3"><i class="fas fa-download bc pr-2"></i> <span class="bf">Télécharger la charte</span></a>
+                        </div>
+                    </div>
+                
+                </div>
+            </div>
+            <!------------------------------------->
+            <!-- Une raison d'être -->
+            <!------------------------------------->
+            <div class="block-2">
+            <div class="block d-md-flex">
+
+                    <div class="text">
+                        <h3 class="text-uppercase">Une raison d'être</h3>
+                
+                        <div class="mt-4">
+                            <p>La Bascule a pour ambition de <span class="bf">pousser notre société vers un modèle viable et respectueux de la nature et de l'Homme</span></p>
+                            <p>Nous agissons pour :</p>
+                            <ul class="list-style-custom">
+                                <li><i class="arrow-right fas fa-arrow-right bc mr-2"></i> <span class="bf">L'évolution de nos consciences</span> vers plus d'empathie avec le vivant dont nous faisons partie ainsi que les générations futures</li>
+                                <li><i class="arrow-right fas fa-arrow-right bc mr-2"></i> <span class="bf">L'amplification des solutions</span> existantes qui permettent une transition démocratique, écologique et solidaire</li>
+                                <li><i class="arrow-right fas fa-arrow-right bc mr-2"></i> <span class="bf">La protection du vivant et de sa génération</span></li>
+                                <li><i class="arrow-right fas fa-arrow-right bc mr-2"></i> La réappropriation par le citoyen du <span class="bf">pouvoir d'agir et de décider</span></li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div class="text bgbc">
+                        <div class="row mt-4">
+                            <div class="col-3">
+                                <img src="<?php echo get_template_directory_uri(); ?>/img/gouvernance-partagee.svg" alt="logo gouvernance">
+                            </div>
+                            <div class="col-9">
+                                <span class="bf">Gouvernance partagée</span>
+                                <p>Fonctionner en gouvernance partagée pour aller <span class="font-weight-bold">du pouvoir sur</span> des personnes <span class="font-weight-bold">au pouvoir de</span> faire avec laisser place à la puissance de l'intelligence collective.</p>
+                            </div>
+                        </div>
+            
+                        <div class="row mt-4">
+                            <div class="col-3">
+                                <img src="<?php echo get_template_directory_uri(); ?>/img/mobilisation.svg" alt="logo force de mobilisation">
+                            </div>
+                            <div class="col-9">
+                                <span class="bf">Force de mobilisation</span>
+                                <p>Mobiliser un maximum de citoyens.</p>
+                            </div>
+                        </div>
+            
+                        <div class="row mt-4 mb-4">
+                            <div class="col-3">
+                                <img src="<?php echo get_template_directory_uri(); ?>/img/independance-financiere.svg" alt="logo independance financiere">
+                            </div>
+                            <div class="col-9">
+                                <span class="bf">Indépendance financière</span>
+                                <p>Atteindre l'indépendance financière dans notre fonctionnement au quotidien.</p>
+                            </div>
+                        </div>
+                    </div>                    
+
+            </div>
+            </div>
+            <!------------------------------------->
+            <!-- Une démarche perso -->
+            <!------------------------------------->
+            <div class="block-3">
+            <div class="block d-md-flex">
+
+                    <div class="image">
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/p_association/prog-actions.svg" alt="action-prog">
+                    </div>
+
+                    <div class="text">
+                        <h3 class="text-uppercase mt-4">Une démarche personnelle<br>d'engagements</h3>
+                        <div class="mt-4">
+                            <p>pour faire la bascule avec ou sans nous</p>
+                        </div>
+            
+                        <div class="mt-5">
+                            <a href="" class="btn btn-primary">En savoir +</a>
+                        </div>
+                    </div>
+
+            </div>
+            </div>
+            <!------------------------------------->
+            <!-- Philosophie de coopération -->
+            <!------------------------------------->
+            <div class="block-4">
+            <div class="block d-md-flex">
+
+                    <div class="text">
+                        <h3 class="text-uppercase mt-4">Une philosophie de coopération</h3>
+                        
+                        <div class="mt-4">
+                            <p>pour que nous, citoyens et organisations, puissions bâtir une unité dans nos diversités.</p>
+                        </div>
+
+                        <div class="mt-5">
+                            <a href="" class="btn btn-primary">Plonger dans l'archipel</a>
+                        </div>
+                    </div>
+
+                    <div class="image bgj">
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/p_association/archipel.jpg" alt="archipel">
+                    </div>   
+
+            </div>
+            </div>
+            <!------------------------------------->
+            <!-- Une équipe -->
+            <!------------------------------------->
+            <div class="block-5">
+            <div class="block d-md-flex">
+
+                    <div class="image"></div>
+
+                    <div class="text">
+                        <h3 class="text-uppercase mt-4">Une équipe</h3>
+                        <div class="mt-4">
+                            <p>portée par une jeunesse qui a su bien s'entourer.</p>
+                            <p><span class="bf">Des citoyens de tout âges</span> en quête de sens qui apportent recul et expériences.</p>
+                            <p><span class="bf">Des professionnels engagés de tout horizons</span> qui donnent de leur temps pour transmettre leur savoir-faire.</p>
+                        </div>
+
+                        <div class="mt-5">
+                            <a href="" class="btn btn-primary">Faire connaissance avec l'équipe</a>
+                        </div>
+                    </div>   
+
+            </div>
+            </div>
+            <!------------------------------------->
+            <!-- Des lieux de bascule -->
+            <!------------------------------------->
+            <div class="block-6">
+            <div class="block d-md-flex">
+
+                    <div class="text">
+                        <h3 class="mt-4"><span class="text-uppercase">Des lieux</span> de bascule</h3>
+
+                        <div class="mt-4">
+                            <p>Espaces uniques de vie-travail-transformation, accueillant pendant plusieurs semaines des volontaires à plein temps pour:</p>
+                            <ul class="list-style-custom">
+                                <li><i class="arrow-right fas fa-arrow-right bc mr-2"></i><span class="bf text-uppercase">Agir</span> au service des solutions existantes</li>
+                                <li><i class="arrow-right fas fa-arrow-right bc mr-2"></i><span class="bf text-uppercase">Apprendre</span> à vivre et faire ensemble</li>
+                                <li><i class="arrow-right fas fa-arrow-right bc mr-2"></i><span class="bf text-uppercase">Grandir</span> son engagement</li>
+                            </ul>
+                        </div>
+
+                        <div class="mt-5">
+                            <a href="" class="btn btn-primary">En savoir +</a>
+                        </div>
+                    </div>
+
+
+                    <div class="text bg-team">
+                        <h3><span class="text-uppercase">Une ambition</span> d'<span class="text-uppercase">implantations</span> sur le territoire</h3>
+                        
+                        <div class="mt-4">
+                            <p>Notre objectif :</p>
+                            <p>passer en deux ans de 3 à 30 lieux de bascule répartis sur autant de communes différentes; et d'une équipe de 50 à 300 volontaires à temps plein, secondés sur le territoire par de nombreux citoyens engagés.</p>
+                        </div>
+
+                        <div class="mt-5">
+                            <a href=""><i class="fas fa-download bc pr-2"></i> <span class="bf">Voir notre projet de développement</span></a>
+                        </div>
+                    </div>
+                    
+            </div>
             </div>
 
-            <div class="col-6">
-                <img src="" alt="logo">
+
+
+<!---------------------------------------------------------------------------->
+<!-- Content - Part.2 -->
+<!------------------------------------->
+
+<!-- Title - Section 2 -->
+<!------------------------------------->
+<div class="site-section bg-light">
+    <div class="container">
+          <div class="heading pb-5">
+                <h1 class="text-center text-uppercase">Chiffres clés</h1>
+          </div>
+    
+    <!-- UserCom - Section 2 -->
+    <!------------------------------------->
+        <div class="row pt-4">
+    <!-- UserCom - Citoyens --> 
+          <div class="col-md-6 col-lg-3">
+          <div class="block-user">
+    
+                <div class="vcard text-center">
+                    <div class="image">
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/citoyens.svg" alt="petit logo top">
+                    </div>
+                    <div class="name-text">
+                        <h1 class="pt-4">000</h1>
+                    </div>
+                </div>
+    
+                <div class="text text-center">
+                    <p><span class="bf">Citoyens</span> passés dans un lieu de bascule</p>
+                </div>
+          </div>
+          </div>
+    
+    <!-- UserCom - Lieux --> 
+          <div class="col-md-6 col-lg-3">
+          <div class="block-user">
+    
+                <div class="vcard text-center">
+                    <div class="image">
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/lieux.svg" alt="petit logo top">
+                    </div>
+
+                    <div class="name-text">
+                        <h1 class="pt-4">3</h1>
+                    </div>
+                </div>
+    
+                <div class="text text-center">
+                    <ul class="list-unstyled">
+                        <li><span class="bf">Joigny</span> (89), à l'echelle nationale</li>
+                        <li><span class="bf">Plouray</span> (22), à l'echelle régionale</li>
+                        <li><span class="bf">Rennes</span> (22), à l'echelle locale</li>
+                    </ul>
+                </div>
+          </div>
+          </div>
+    
+    <!-- UserCom - Events -->           
+          <div class="col-md-6 col-lg-3">
+          <div class="block-user">
+    
+                <div class="vcard text-center">
+                    <div class="image">
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/event.svg" alt="petit logo top">
+                    </div>
+                    <div class="name-text">
+                        <h1 class="pt-4">000</h1>
+                    </div>
+                </div>
+    
+                <div class="text text-center">
+                    <p><span class="bf">Evénements animés</span></br> dans le cadre de</p>
+                    <ul class="list-unstyled">
+                        <li>*formations*</li>
+                        <li>*rencontres*</li>
+                        <li>*ateliers de travail*</li>
+                    </ul>
+                </div>
             </div>
-        </div>
-    </div>
-
-    <div class="row text-center">
-        <div class="m-auto col-4">
-            <h3>Une démarche personelle d'engagement</h3>
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="col-6">
-            <h3>Faire la bascule, c'est...</h3>
-            <p>une démarche personelle d'engagement, pour mener des actions avec ou sans nous</p>
-
-            <div class="row pt-3">
-                <a href="" class="btn">Plonger dans l'Archipel</a>
             </div>
 
-        </div>
-        <div class="col-6 background-img">
-            <p>"S'engager dans une ou plusieurs de ces actions c'est faire la Bascule! "</p>
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="col-6">
-            <h3>Tout le monde peut devenir acteur de la Bascule!</h3>
-            <p>Réveillons le citoyens qui sommeille en nous.</p>
-
-            <div class="row pt-3">
-                <a href="" class="btn">Passer à l'action</a>
+    <!-- UserCom - Organisations -->           
+          <div class="col-md-6 col-lg-3">
+          <div class="block-user">
+    
+                <div class="vcard text-center">
+                    <div class="image">
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/organisations.svg" alt="petit logo top">
+                    </div>
+                    <div class="name-text">
+                        <h1 class="pt-4">000</h1>
+                    </div>
+                </div>
+    
+                <div class="text text-center">
+                    <p><span class="bf">Organisations</span> différentes ont participé à un de vos événements</p>
+                </div>
             </div>
-
-        </div>
-        <div class="col-6">
-            <p>Liste de 5 exemples
-            Relier son club de loisir à l'association zéro déchet du coin
-            Etre en relai de communication des actions de son territoire auprès de ses proches, son entreprise, son école, ses réseaux sociaux.
-            Participer aux actions qui existent sur mon territoire
-            Rejoindre un groupe local d'une association qui me tient à coeur et participer à la connecter à d'autres initiatives
-            Organiser un repas de rencontre pour que différents porteurs de projets de ma connaissance se rencontrent
-            Signaler les lieux vacants, à réhabiliter pour devenir des lieux de bascule </p>
-        </div>
-    </div>
+            </div>
+    
+      </div>
+</div>
 </div>
 
+            
+
+            
+
+    </div>
+</section>
+
+<!---------------------------------------------------------------------------->
+<!-- Content - Part.1 -->
+<!------------------------------------->
+
+
+<!---------------------------------------------------------------------------->
+<!-- FOOTER -->
 <?php get_footer();?>
+<!---------------------------------------------------------------------------->
